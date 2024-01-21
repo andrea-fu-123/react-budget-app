26dc79789f786fc2a0672724b68b94171de9aca7
andreacfu <andrea.cy.fu@gmail.com>
2024-01-21 14:21:15 -0800

    Move authentication into middleware

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,59 @@ const db = mysql.createConnection({
 app.use(express.json())
 app.use(cors())
 
+// Custom middleware function
+app.use('/transactions/:user_id', (req, res, next) => {
+    const token = req.headers['token'];
+
+    // Extract user_id from the URL parameter
+    const user_id = req.params.user_id;
+
+    console.log('Token:', token);
+    console.log('User ID:', user_id);
+
+    if (!token || !user_id) {
+        res.status(401).send("Token or user_id missing");
+        return;
+    }
+
+    next();
+//     // Code to run at the start of every request
+//     const token = req.headers['token'];
+//     console.log(token)
+
+//     // Extract user_id from the URL parameter
+//     const user_id = req.params.user_id;
+
+//     if (!token || !user_id) {
+//         res.status(400).send("Token or user_id missing");
+//         return;
+//     }
+
+//     const decodedToken = jwtDecode(token)
+
+//     const q = "SELECT email FROM users WHERE user_id = ?"
+//     const values = [req.params.user_id]
+//     let userEmail = ""
+//     db.query(q, values, (err, data) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(400).json(err);
+//             return;
+//         } else {
+//             res.status(200).json(data);
+//             userEmail = json(data);
+//         }
+//     });
+
+//     if (decodedToken.email !== userEmail) {
+//         return 403;
+//     }
+//     console.log("hehehehee")
+
+//     // Continue to the next middleware in the stack
+
+});
+
 db.on('error', (err) => {
     console.error('MySQL Connection Error:', err);
 });
@@ -54,18 +107,10 @@ app.get('/users', (req, res) => {
 });
 
 app.get('/transactions/:user_id', (req, res) => {
-    const token = req.headers['token'];
+    // const token = req.headers['token'];
 
-    // Extract user_id from the URL parameter
+    // // Extract user_id from the URL parameter
     const user_id = req.params.user_id;
-
-    if (!token || !user_id) {
-        res.status(400).send("Token or user_id missing");
-        return;
-    }
-
-    const decodedToken = jwtDecode(token); // decode for authorization
-
     const q = 'SELECT * FROM transaction_history WHERE user_id = ? ORDER BY date DESC, amount ASC';
 
     db.query(q, [user_id], (err, data) => {
@@ -81,15 +126,8 @@ app.get('/transactions/:user_id', (req, res) => {
 
 
 app.post('/transactions/:user_id', (req, res) => {
-    const token = req.headers['token'];
 
     // Extract user_id from the URL parameter
-    const user_id = req.params.user_id;
-
-    if (!token || !user_id) {
-        res.status(400).send("Token or user_id missing");
-        return;
-    }
 
     const q = "INSERT INTO transaction_history (date, amount, user_id) VALUES (?,?,?)"
     const values = [req.body.date, req.body.amount, req.params.user_id]
@@ -105,15 +143,6 @@ app.post('/transactions/:user_id', (req, res) => {
 })
 app.delete('/transactions/:user_id', async (req, res) => {
 
-    const token = req.headers['token'];
-
-    // Extract user_id from the URL parameter
-    const user_id = req.params.user_id;
-
-    if (!token || !user_id) {
-        res.status(400).send("Token or user_id missing");
-        return;
-    }
     const q = "DELETE FROM transaction_history WHERE id= ? AND user_id = ?"
     console.log(q)
     const values = [req.body.id, req.params.user_id]
@@ -121,7 +150,7 @@ app.delete('/transactions/:user_id', async (req, res) => {
         if (err) {
             res.status(400).json(err)
             return
-        } 
+        }
         else {
             res.status(200).json(req.body.id)
         }
--- backend/index.test.js
+++ backend/index.test.js
@@ -1,25 +1,20 @@
 import app from './index.js'
-import request from 'supertest'; 
+import request from 'supertest';
 
 describe('Express App Tests', () => {
-    test('Home page should respond with "Connected to backend" on GET /', async () => {
-      const response = await request(app).get('/');
-      expect(response.status).toBe(200);
-      expect(response.text).toBe('"Connected to backend"');
-    });
 
-    test ('GET /transactions', async () => {
-        const getResponse = await request(app).get('/transactions');
-        expect(getResponse).toBeDefined();
-        expect(getResponse.status).toBe(200)
-        
-    })
-    test ('POST - happy case' , async () => {
-        const postResponse = await request(app).post('/transactions').send({
-            date: '2010-02-01',
-            amount: 62039.38
-          })
-        expect(postResponse.body.date).toBe('2010-02-01')
-        expect(postResponse.body.amount).toBe(62039.38)
-      });
-  });
+  test('GET /transactions 401 with no token', async () => {
+    const getResponse = await request(app).get('/transactions/69986e27-981e-45c3-bfc5-300f0af39710');
+    expect(getResponse).toBeDefined();
+    expect(getResponse.status).toBe(401)
+
+  })
+  test('GET /transactions 200 with matching token', async () => {
+    const getResponse = await request(app).get('/transactions/69986e27-981e-45c3-bfc5-300f0af39710').set('token', 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ4YTYzYmM0NzY3Zjg1NTBhNTMyZGM2MzBjZjdlYjQ5ZmYzOTdlN2MiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIyMjUzNDk4OTE5MTItaXRsdDE5dWFzZmFwZjMwZ2RnZDVrMmFodHFxMTc0ajcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIyMjUzNDk4OTE5MTItaXRsdDE5dWFzZmFwZjMwZ2RnZDVrMmFodHFxMTc0ajcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDUwODE3MDQ5MTY1ODE5MzQzNDAiLCJlbWFpbCI6ImU0MDc5ODFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5iZiI6MTcwNTg3Mzk3NSwibmFtZSI6ImVkaXQgbCIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLSFpOcS1WTWxqYVF3aTR2eTVuMzQ1UjYtZF9pSFhUc05EUEhFX1JrZU09czk2LWMiLCJnaXZlbl9uYW1lIjoiZWRpdCIsImZhbWlseV9uYW1lIjoibCIsImxvY2FsZSI6ImVuLUdCIiwiaWF0IjoxNzA1ODc0Mjc1LCJleHAiOjE3MDU4Nzc4NzUsImp0aSI6IjcyYTNmOTQ0N2FhMGZlMmNiODg2ZTI2ZjBlMGMxYTE4MTExZTdkNmQifQ.jBd9inBlBK43UazQeR2x6jmmxagNCDRmazXmZZWJsneiJW2gglFYTA1tWQgI3j79XIQzTafp0nz2ekZLuij2KEhsNgKJL3nn7SIxDkpC2dm-8k8JjrxOvcHMDc4pLLi6WRFPsP9P7BYGDXeIGtM8RhXOaiT2LyA664jcuEIfNOEb7E3AytmxNA7HnEuRCrnBN3LocFsl2ApvKzxFgiF8W7ADNS6JRYeaL0JB-RQM3ob8RkvcPk8ny5crgQYVkb1BTvCLLc9fesnmWj2dwSLeECF3DS9pTwMk0Fav9C7cX4tGY42Ot7ZSuCCntSR3AcCnxewMUhdy2SHMJ9uAqZw6wg');
+    expect(getResponse).toBeDefined();
+    expect(getResponse.status).toBe(200)
+
+  })
+
+
+});
